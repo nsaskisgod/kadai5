a25b73bf41608850a0d9f8a4b0f89ea917840e73
Sorachi <j1315@PC4-52.cc.matsue-ct.jp>
2017-09-26 16:35:34 +0900

    壁にぶつかったら止まるように変更

--- player.rb
+++ player.rb
@@ -1,8 +1,34 @@
 # coding: utf-8
 
 class Player < Sprite
+
   def update
-    self.x += Input.x
-    self.y += Input.y
+    #入力方向に移動
+    self.x += Input.x*3
+    self.y += Input.y*3
+
+    #画面下には行けない
+    if self.y >= Window.height - self.image.height
+      self.y -= 3
+    end
+    #画面上には行けない
+    if self.y <= 0
+      self.y += 3
+    end
+    #画面右に行けない
+    if self.x >= Window.width - self.image.width
+      self.x -= 3
+    end
+    #画面左には行けない
+    if self.x <= 0
+      self.x += 3
+    end
   end
+
+  # 他のオブジェクトから衝突された際に呼ばれるメソッド
+  def hit(obj)
+    self.x -= Input.x*3
+    self.y -= Input.y*3
+  end
+
 end
