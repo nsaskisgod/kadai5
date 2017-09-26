b32f3ca2fd06f3d745084d4bfae4d7bb2672b663
Sorachi <j1315@PC4-52.cc.matsue-ct.jp>
2017-09-26 16:32:08 +0900

    画面外に出ないように変更

--- enemy.rb
+++ enemy.rb
@@ -1,15 +1,38 @@
 # coding: utf-8
 
 class Enemy < Sprite
+
   def update
-    self.y += 1
-    if self.y >= Window.height - self.image.height
-      self.vanish
+
+    self.y += rand(7)-3
+
+    #画面下には行けない
+    if self.y >= Window.height - self.image.height - 50
+      self.y -= 1
     end
+    #画面上には行けない
+    if self.y <= 50
+      self.y += 1
+    end
+    #画面右に行けない
+    if self.x >= Window.width - self.image.width - 50
+      self.x -= 1
+    end
+    #画面左には行けない
+    if self.x <= 50
+      self.x += 1
+    end
+
   end
 
+=begin
   # 他のオブジェクトから衝突された際に呼ばれるメソッド
   def hit(obj)
     self.vanish
   end
+=end
+  def hit(obj)
+    self.vanish
+  end
+
 end
